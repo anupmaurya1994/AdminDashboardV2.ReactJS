3ecafa310e3a188fedba6333223d00bdd2996938
Anup Maurya <anup.maurya484@ggmaill.com>
2024-07-08 12:31:34 +0530

    First commit

--- Server/config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    BASE_URL : process.env.BASE_URL || 'http://localhost:3004',
-    MONGO_DB : process.env.DB || 'mongodb://127.0.0.1:27017/admin-panel',
-}
--- Server/model/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const config = require('../config/config')
-const userModel = require('./userModel')
-const db = {}
-db.mongoose = mongoose
-db.mongodb = config.MONGO_DB
-db.userModel = userModel
-module.exports = db
